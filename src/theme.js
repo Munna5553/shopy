789dad1e5a0fafcda8af90a315db34eec05fc062
munna <shauryakumar42040er@gmail.com>
2023-11-15 20:03:01 +0530

    material ui config

---
+++ src/theme.js
@@ -0,0 +1,48 @@
+import { createTheme } from "@mui/material";
+const theme = createTheme({
+    palette: {
+        background: {
+            default: '#ffffff', // body color
+        },
+        primary: {
+            main: '#ec3946', // main color
+        },
+        secondary: {
+            main: '#fcebec', // secondary color
+        },
+        tertiary: {
+            main: '#a8dadc', // tertiary color
+        },
+    },
+    typography: {
+        fontFamily: [
+            'Roboto',
+            'sans-serif',
+            'Arial',
+            'Helvetica',
+            'serif',
+        ].join(','),
+        h1: {
+            fontFamily: '"Playfair Display", serif', // heading font
+            fontSize: '4rem', // main heading size
+            fontWeight: 700,
+        },
+        h2: {
+            fontFamily: '"Playfair Display", serif', // heading font
+            fontSize: '2.5rem', // section heading size
+            fontWeight: 600,
+        },
+        h3: {
+            fontFamily: '"Playfair Display", serif', // heading font
+            fontSize: '1.5rem', // heading size
+            fontWeight: 500,
+        },
+        fontFamily: '"Playfair Display", serif', // heading font
+
+        body1: {
+            fontSize: '1rem', // default text size
+        },
+    },
+});
+
+export default theme;
